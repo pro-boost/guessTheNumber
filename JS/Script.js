337b56c828c8b2ac899e0bd2d36b128646d7bc8a
MedBrk <150671350+med-ctrl@users.noreply.github.com>
2024-03-22 20:28:04 +0000

    Updated game logic: - Ensure a new random number is generated after each round - Reset attempts to 0 when reaching 10 attempts - Implement game restart functionality after 10 attempts

--- JS/Script.js
+++ JS/Script.js
@@ -1,3 +1,56 @@
-function helloWorld() {
-  console.log "Hello World!"
+let randomNumber;
+let guessInput = document.getElementById("guess");
+let result;
+let attempts = 0;
+let score = 0;
+
+// Function to generate a random number between 1 and 100
+function generateRandomNumber() {
+        randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+// Function to compare the player's guess with the random number;
+function playGame(){    
+    let guess = parseInt(guessInput.value); // Getting value from input field
+    console.log(randomNumber);
+
+    if (0 < guess && guess < randomNumber) {
+        result = "Wrong! your guess was low!";
+        attempts++;
+    } else if (100 >= guess && guess > randomNumber) {
+        result = "Wrong! your guess was high!";
+        attempts++;
+    } else if (guess === randomNumber) {
+        result = "Congratulations! you got it right!";
+        score++;
+        attempts = 0; // Reset attempts to zero
+        randomNumber = null; // Reset the random number
+        generateRandomNumber(); // Generate a new random number for the next round
+    } else {
+        result = "Invalid input! Please enter a number between 1 and 100.";
+    
+    }
+    if (attempts === 10) {
+        result = "You have reached the maximum number of attempts. You lose!";
+        score = 0;
+        generateRandomNumber(); // Generate a new random number for the next round
+        document.getElementById('previous-guesses').innerText = "Previous guesses : " ; // Reset previous guesses
+        document.getElementById('attempts').innerText; // Reset attempts
+        attempts = 0; // Reset attempts to zero
+    }
+    
+    
+    document.getElementById('previous-guesses').innerText += " "+ guess + " "; // displaying all previous guesses
+    document.getElementById('result').innerText = result;
+    document.getElementById('attempts').innerText = "Failed attempts : " + attempts;
+    document.getElementById('score').innerText = "The score is " + score;
+}
+
+// Call generateRandomNumber function when the page loads
+window.onload = generateRandomNumber;
+
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        playGame();
+    }
 }
